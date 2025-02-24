7456ec2db1789b08151b595b52830e03a5eedcbd
EunGyeoll <162575611+EunGyeoll@users.noreply.github.com>
2025-02-24 13:03:06 +0900

    [Bronze IV] Title: 숫자의 합, Time: 172 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,20 +1,20 @@
-import java.io.*;
 import java.util.Scanner;
-
-
+ 
 public class Main {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        String num = sc.next();
-
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            sum+=num.charAt(i)-48;
-        }
-        System.out.println(sum);
-    }
+	public static void main(String[] args) {		
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+		String a = in.next();
+		in.close();
+		
+		int sum = 0;
+        
+		for(int i = 0; i < N; i++) {
+			sum += a.charAt(i)-'0';
+            // charAt() 은 문자의 아스키코드 값을 반환하므로 반드시 -48 또는 -'0' 을 해야 입력받은 숫자 값 그대로를 사용할 수 있다.
+		}
+		System.out.print(sum);
+	}
 }
+ 
